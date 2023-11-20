8b5c78f1d0a8fd586ec126b4ad7f2844263a02f2
guga-ggw <ggwomega15@gmail.com>
2023-11-20 23:10:20 +0400

    k

--- form/src/App.tsx
+++ form/src/App.tsx
@@ -1,58 +1,106 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { FieldValues, useForm } from 'react-hook-form';
 import { ZodType, z } from 'zod';
 
 function App() {
-  type form = {
-    firstName: string;
-    lastName: string;
-    email: string;
-    age: number;
-    password: string;
-    confirmPassword: string;
-  };
+  const {
+    register,
+    handleSubmit,
+    formState : {isSubmitting, errors},
+    reset,
+    getValues
+  } = useForm()
 
-  const schema: ZodType<form> = z.object({
-    firstName: z.string().min(2).max(30),
-    lastName: z.string().min(2).max(30),
-    email: z.string().email(),
-    age: z.number().min(18).max(100),
-    password: z.string().min(8).max(20),
-    confirmPassword: z.string().min(8).max(20)
-  }).refine((data) => data.password === data.confirmPassword, {
-    message: 'Passwords do not match',
-    path: ['confirmPassword']
-  });
-
-  const { register, handleSubmit, formState: { errors } } = useForm<form>({
-    resolver: zodResolver(schema)
-  });
-
-  const SubmitData = (data: form) => {
-    console.log(data);
-  };
+  const onSubmit = async (data: FieldValues) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+    reset()
+  }
 
   return (
     <>
-      <form action="" id="Form" onSubmit={handleSubmit(SubmitData)}>
+      <form onSubmit={handleSubmit(onSubmit)} action="" id="Form" >
         <div className="header">Register</div>
-        <div className="inputs">
+        <div className="inputs"
+        >
           <label htmlFor="firstName">FirstName</label>
-          <input type="text" {...register('firstName')} />
-          {errors.firstName && <label>{errors.firstName.message}</label>}
+          <input type="text" 
+            {
+              ...register('firstName', {
+                required : "firstname is required",
+                minLength : {
+                  value : 2,
+                  message : "firstName should me at least 2 characters"
+                }
+              })
+            }
+          />
+          {errors.firstName && <p>{errors.firstName.message}</p>}
           <label htmlFor="lastName">LastName</label>
-          <input type="text" {...register('lastName')} />
+          <input type="text" 
+          {
+            ...register('lastName', {
+              required : "LastName is required",
+              minLength : {
+                value : 2,
+                message : "lastName should me at least 2 characters"
+              }
+            })
+          }
+          />
+          {errors.lastName && <p>{errors.lastName.message}</p>}
           <label htmlFor="email">Email</label>
-          <input type="email" {...register('email')} />
+          <input type="email" 
+            {
+              ...register('email', {
+                required : "email is required",
+
+              })
+            }
+          />
           <label htmlFor="age">Age</label>
-          <input type="number" {...register('age')} />
+          <input type="number"
+            {
+              ...register('age', {
+                min : {
+                  value : 18,
+                  message : "You should me 18 or higher"
+                }
+              })
+            }
+          />
+           {errors.age && <p>{errors.age.message}</p>}
           <label htmlFor="password">Password</label>
-          <input type="password" {...register('password')} />
+          <input type="password"  
+            {
+              ...register('password', {
+                minLength : {
+                  value : 8,
+                  message : "Password must be at least 8 characters"
+                }
+              })
+            }
+          />
+          {errors.password && <p>{errors.age.password}</p>}
           <label htmlFor="confirmPassword">Confirm password</label>
-          <input type="password" {...register('confirmPassword')} />
+          <input type="password" 
+            {
+              ...register('confirmPassword', {
+                minLength : {
+                  value : 8,
+                  message : "must be at least 8 characters"
+                },
+                validate : (value) => 
+                  value === getValues('password') || "Passwords do not much"
+                
+              })
+            }
+          />
+          {errors.confirmPassword && <p>{errors.age.confirmPassword}</p>}
         </div>
-        <button type="submit" onClick={() => handleSubmit(SubmitData)}>Submit</button>
+        <button 
+        disabled={isSubmitting}
+        type="submit">Submit</button>
       </form>
     </>
   );
